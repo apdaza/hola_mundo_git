e29cf2d06164817f4aedacd42c03c46e9b8aa9d1
apdaza <apdaza@gmail.com>
2018-08-09 06:37:09 -0500

    hola_mundo v3 corregida la estructura básica del programa

--- hola_mundo.java
+++ hola_mundo.java
@@ -1,5 +1,7 @@
 
 //Primer contacto con java
-public class HolaMundo(String[] args){
-	System.out.println("Hola Mundo");
+public class HolaMundo{
+	public static void main(String[] args){
+		System.out.println("Hola Mundo");
+	}
 }
